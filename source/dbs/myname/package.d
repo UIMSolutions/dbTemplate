f0a32f5148e7f6e9b64129b9f4bce7581741d100
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-07 15:02:06 +0200

    Updates

--- source/dbs/myname/package.d
+++ source/dbs/myname/package.d
@@ -1,1 +1,12 @@
 module dbs.myname;
+
+public { // required uim libraries
+  import uim.core;
+  import uim.oop;
+  import uim.dbs;
+}
+
+public { // Own libraries
+  import dbs.myname.helpers;
+  import dbs.myname.tests;
+}
